0393b7b6d50f153e359cacc7bc17e728b2270754
ToddJMullen <ToddJ.Mullen@Gmail.com>
2019-08-17 01:45:58 -0400

    Close scanner resource

--- SecretMessages/src/SecretMessages.java
+++ SecretMessages/src/SecretMessages.java
@@ -17,7 +17,7 @@ public class SecretMessages {
 		for( int idx = 0; idx < str.length(); idx++ ) {
 			
 			char input = str.charAt(idx);
-			
+
 			if( input >= 'A' && input <= 'Z' ) {
 				input += key;
 				
@@ -25,11 +25,19 @@ public class SecretMessages {
 					input -= 26;//shift it back into the uppercase range
 				}
 				
+			} else if( input >= 'a' && input <= 'z' ) {
+				input += key;
+				
+				if( input > 'z' ) {
+					input -= 26;//shift it back into the lowercase range
+				}
+				
 			}
 			output += input;
 		}
 		
 		System.out.println("Output:\n" + output);
+		cli.close();
 
 	}//main
 
