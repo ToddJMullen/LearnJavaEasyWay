4a50f1c5d82ca03969589fcd2ad6a9355ade50c3
ToddJMullen <ToddJ.Mullen@Gmail.com>
2019-08-25 17:38:37 -0400

    SM Android, Base version SecretMessages app. - Bind encode / decode button & text areas - Bind SeekBar to the key on change

--- SecretMessages2/app/src/main/java/com/example/secretmessages/MainActivity.java
+++ SecretMessages2/app/src/main/java/com/example/secretmessages/MainActivity.java
@@ -11,8 +11,17 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity{
+
+	EditText tiKey;
+	EditText taInput;
+	EditText taOutput;
+	SeekBar slKey;
+	Button btnEncode;
 
 	@Override
 	protected void onCreate( Bundle savedInstanceState ){
@@ -20,6 +29,40 @@ public class MainActivity extends AppCompatActivity{
 		setContentView(R.layout.activity_main);
 		Toolbar toolbar = findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
+
+		tiKey		= (EditText)	findViewById(R.id.tiKey);
+		taInput		= (EditText)	findViewById(R.id.taInput);
+		taOutput	= (EditText)	findViewById(R.id.taOutput);
+		slKey		= (SeekBar)		findViewById(R.id.slKey);
+		btnEncode	= (Button)		findViewById(R.id.btnEncode);
+
+		btnEncode.setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick( View view ){
+				int key = Integer.parseInt(tiKey.getText().toString());
+//				taOutput.setText( tiKey.getText().toString() );// << for testing
+				encode( key );
+			}
+		});
+		slKey.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+			@Override
+			public void onProgressChanged( SeekBar seekBar, int i, boolean b ){
+				int key = slKey.getProgress() - 13;
+				tiKey.setText( "" + key );
+				encode( key );
+			}
+
+			@Override
+			public void onStartTrackingTouch( SeekBar seekBar ){
+
+			}
+
+			@Override
+			public void onStopTrackingTouch( SeekBar seekBar ){
+
+			}
+		});
+
 
 		FloatingActionButton fab = findViewById(R.id.fab);
 		fab.setOnClickListener(new View.OnClickListener(){
@@ -29,14 +72,85 @@ public class MainActivity extends AppCompatActivity{
 						.setAction("Action", null).show();
 			}
 		});
-	}
+	}//onCreate/
+
+
+	private void encode( int key ) {
+
+		String plaintext = taInput.getText().toString();
+
+//		if( plaintext.trim().length() == 0 ) {
+//			JOptionPane.showMessageDialog(getContentPane(), "You haven't entered anything to be encoded!");
+//			return;
+//		}
+
+		String ciphertext = caesar(plaintext, key);
+
+		ciphertext = stringReverse(ciphertext);
+
+		taOutput.setText(ciphertext);
+	}//encode/
+
+	private static String stringReverse( String str ) {
+		String rts = "";
+		for( int l = str.length()-1; l >= 0; l-- ) {
+			rts += str.charAt(l);
+		}
+		return rts;
+	}//stringReverse/
+
+
+
+
+	private static String caesar( String str, int keyVal ) {
+		char key = (char) keyVal;
+		String csr = "";
+		for( int i = 0; i < str.length(); i++ ) {
+			char input = str.charAt(i);
+			if( input >= 'A' && input <= 'Z' ) {
+				input += key;
+
+				if( input > 'Z' ) {
+					input -= 26;//shift it back into the uppercase range
+				}
+				if( input < 'A' ) {
+					input += 26;
+				}
+
+			}
+			else if( input >= 'a' && input <= 'z' ) {
+				input += key;
+
+				if( input > 'z' ) {
+					input -= 26;//shift it back into the lowercase range
+				}
+				if( input < 'a' ) {
+					input += 26;
+				}
+			}
+			else if( input >= '0' && input <= '9' ) {
+				input += (keyVal % 10);
+				if( input > '9' ) {
+					input -= 10;
+				}
+				else if( input < '0' ) {
+					input += 10;
+				}
+			}
+			csr += input;
+		}
+
+		return csr;
+	}//caesar/
+
+
 
 	@Override
 	public boolean onCreateOptionsMenu( Menu menu ){
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.menu_main, menu);
 		return true;
-	}
+	}//onCreateOptionsMenu/
 
 	@Override
 	public boolean onOptionsItemSelected( MenuItem item ){
@@ -51,5 +165,9 @@ public class MainActivity extends AppCompatActivity{
 		}
 
 		return super.onOptionsItemSelected(item);
-	}
-}
+	}//onOptionsItemSelected/
+
+
+
+
+	}//MainActivity
