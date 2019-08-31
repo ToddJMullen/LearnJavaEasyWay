48b901c574a8d191243af852f8e77eeb04b4b30b
ToddJMullen <ToddJ.Mullen@Gmail.com>
2019-08-31 17:19:48 -0400

    BubbleDraw, Desktop version 1 - Add click, drag, & mouse wheel listeners for drawing bubbles & altering size. - Convert height & width to configurable constants.

--- BubbleDraw/src/BubbleDraw.java
+++ BubbleDraw/src/BubbleDraw.java
@@ -3,13 +3,16 @@ import java.awt.Dimension;
 import javax.swing.JFrame;
 
 public class BubbleDraw extends JFrame {
+	
+	public static final int WIDTH	= 900;
+	public static final int HEIGHT	= 800;
 
 	public static void main(String[] args) {
 
 		JFrame frame = new JFrame("Todd's BubbleDraw App");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().add(new BubblePanel());
-		frame.setSize(new java.awt.Dimension(600,400));
+		frame.getContentPane().add(new BubblePanel(WIDTH,HEIGHT));
+		frame.setSize(new java.awt.Dimension(WIDTH,HEIGHT));
 		frame.setVisible(true);
 	}//main
 
--- BubbleDraw/src/BubblePanel.java
+++ BubbleDraw/src/BubblePanel.java
@@ -1,6 +1,6 @@
+import java.awt.event.*;
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.font.GraphicAttribute;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -8,32 +8,72 @@ import javax.swing.JPanel;
 
 public class BubblePanel extends JPanel {
 	
+	private int WIDTH;
+	private int HEIGHT;
+	
 	Random rand = new Random();
 	ArrayList<Bubble> bubbleList;
 	int size = 25;
 	
-	public BubblePanel() {
+	
+	public BubblePanel( int width, int height) {
+		WIDTH = width;
+		HEIGHT = height;
 		bubbleList = new ArrayList<BubblePanel.Bubble>();
 		setBackground(Color.BLACK);
-		testBubbles();
-	}
+//		testBubbles();
+		addMouseListener( new BubbleListener() );//bind mouse click events to event delegate
+		addMouseMotionListener( new BubbleListener() );//bind mouse drag / move events
+		addMouseWheelListener( new BubbleListener() );//bind wheel movements
+	}//BubblePanel/
+	
+	
 	
 	public void paintComponent( Graphics canvas ) {
 		super.paintComponent(canvas);
 		for( Bubble b: bubbleList ) {
 			b.draw(canvas);
 		}
-	}
+	}//paintComponent/
+	
 	
 	public void testBubbles() {
-		for( int n = 0; n < 100; n++ ) {
-			int x = rand.nextInt(600);
-			int y = rand.nextInt(400);
+		for( int n = 0; n < 300; n++ ) {
+			int x = rand.nextInt( WIDTH );
+			int y = rand.nextInt( HEIGHT );
 			int size = rand.nextInt(50);
 			bubbleList.add( new Bubble( x, y, size) );
 		}
 		repaint();
-	}
+	}//testBubbles/
+	
+	
+	
+	//create mouse event delegate class to encapsulate custom event driven behaviors
+	private class BubbleListener extends MouseAdapter{
+		
+		public void mousePressed( MouseEvent e) {
+			bubbleList.add( new Bubble(e.getX(), e.getY(), size) );
+			repaint();
+		}//mousePressed/
+		
+		public void mouseDragged( MouseEvent e ) {
+			bubbleList.add( new Bubble(e.getX(), e.getY(), size) );
+			repaint();
+		}//mouseDragged/
+		
+		public void mouseWheelMoved( MouseWheelEvent e ) {
+			
+			int dir = System.getProperty("os.name").startsWith("Mac") ? -1 : 1;//normalize mac
+			
+			size += dir * e.getUnitsToScroll();
+		}
+		
+	}//BubbleListener
+	
+	
+	
+	
 	
 	
 	private class Bubble{
@@ -41,6 +81,7 @@ public class BubblePanel extends JPanel {
 		private int y;
 		private int size;
 		private Color color;
+		
 		
 		public Bubble( int newX, int newY, int newSize ){
 			x = newX;
@@ -54,6 +95,7 @@ public class BubblePanel extends JPanel {
 					
 			
 		}//Bubble()
+		
 		
 		public void draw( Graphics canvas ) {
 			canvas.setColor(color);
