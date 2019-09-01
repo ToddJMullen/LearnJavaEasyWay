a85de2f0f7f7e51ceb066bf56b691278642ca1e2
ToddJMullen <ToddJ.Mullen@Gmail.com>
2019-08-31 21:25:57 -0400

    BubbleDraw, Copy project to PixelDraw which constrains positions to 'whole bubble' increments

---
+++ PixelDraw/src/BubblePanel.java
@@ -0,0 +1,121 @@
+import java.awt.event.*;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.Random;
+
+import javax.swing.JPanel;
+
+public class BubblePanel extends JPanel {
+
+	private static final int MIN_BUBBLE_SIZE = 3;
+	private static final int MAX_BUBBLE_SIZE = 300;
+	
+	private int WIDTH;
+	private int HEIGHT;
+	
+	
+	Random rand = new Random();
+	ArrayList<Bubble> bubbleList;
+	int size = 25;
+	
+	
+	public BubblePanel( int width, int height) {
+		WIDTH = width;
+		HEIGHT = height;
+		bubbleList = new ArrayList<BubblePanel.Bubble>();
+		setBackground(Color.BLACK);
+		testBubbles();
+		addMouseListener( new BubbleListener() );//bind mouse click events to event delegate
+		addMouseMotionListener( new BubbleListener() );//bind mouse drag / move events
+		addMouseWheelListener( new BubbleListener() );//bind wheel movements
+	}//BubblePanel/
+	
+	
+	
+	public void paintComponent( Graphics canvas ) {
+		super.paintComponent(canvas);
+		for( Bubble b: bubbleList ) {
+			b.draw(canvas);
+		}
+	}//paintComponent/
+	
+	
+	public void testBubbles() {
+		for( int n = 0; n < 300; n++ ) {
+			int x = rand.nextInt( WIDTH );
+			int y = rand.nextInt( HEIGHT );
+			int size = rand.nextInt( MAX_BUBBLE_SIZE );
+			if( size < MIN_BUBBLE_SIZE ) {
+				size = MIN_BUBBLE_SIZE;
+			}
+			bubbleList.add( new Bubble( x, y, size) );
+		}
+		repaint();
+	}//testBubbles/
+	
+	
+	
+	//create mouse event delegate class to encapsulate custom event driven behaviors
+	private class BubbleListener extends MouseAdapter{
+		
+		public void mousePressed( MouseEvent e) {
+			bubbleList.add( new Bubble(e.getX(), e.getY(), size) );
+			repaint();
+		}//mousePressed/
+		
+		public void mouseDragged( MouseEvent e ) {
+			bubbleList.add( new Bubble(e.getX(), e.getY(), size) );
+			repaint();
+		}//mouseDragged/
+		
+		public void mouseWheelMoved( MouseWheelEvent e ) {
+			
+			int dir = System.getProperty("os.name").startsWith("Mac") ? -1 : 1;//normalize mac
+			
+			size += dir * e.getUnitsToScroll();
+			if( size < MIN_BUBBLE_SIZE ) {
+				size = MIN_BUBBLE_SIZE;
+			}
+			else if( size < MAX_BUBBLE_SIZE ) {
+				size = MAX_BUBBLE_SIZE;
+			}
+		}
+		
+	}//BubbleListener
+	
+	
+	
+	
+	
+	
+	private class Bubble{
+		private int x;
+		private int y;
+		private int size;
+		private Color color;
+		
+		
+		public Bubble( int newX, int newY, int newSize ){
+			x = (newX / newSize) * newSize + newSize/2;
+			y = (newY / newSize) * newSize + newSize/2;
+			size = newSize;
+			color = new Color(
+				rand.nextInt(256)
+				,rand.nextInt(256)
+				,rand.nextInt(256)
+			);
+					
+			
+		}//Bubble()
+		
+		
+		public void draw( Graphics canvas ) {
+			canvas.setColor(color);
+//			canvas.fillOval( x-size/2, y-size/2, size, size );
+			canvas.fillRect( x-size/2, y-size/2, size, size );
+		}
+		
+	}//Bubble
+
+}///BubblePanel
